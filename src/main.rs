008ac19cc7d1514110efcde7436131dece6a8b32
madeinheaven91 <exspectanss@gmail.com>
2024-12-15 18:49:33 +0300

    a bit of bugfixing and cosmetic changes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
+use std::thread;
+
 use crossterm::{
-    event::{self, Event, KeyCode},
+    event::{self, Event, KeyCode, KeyModifiers},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use ratatui::{
-    backend::CrosstermBackend, layout::{Constraint, Flex, Layout, Rect}, style::Stylize, text::ToText, widgets::{Block, Paragraph, Wrap}, Frame
+    backend::CrosstermBackend,
+    layout::{Constraint, Flex, Layout, Rect},
+    style::Stylize,
+    widgets::{Block, Paragraph, Wrap},
+    Frame,
 };
 use tungstenite::{connect, Message};
 
-const URL: &str = "ws://127.0.0.1:3001/ws";
 const TEMPLATE_BEGIN: &str = r#"{"General":{"action":""#;
 const TEMPLATE_END: &str = r#""}}"#;
 
@@ -19,16 +24,38 @@ fn main() -> Result<(), std::io::Error> {
     let mut terminal = ratatui::Terminal::new(backend)?;
     let mut state = State::new();
 
-    let (mut socket, response) = connect(URL).expect("Can't connect");
+    let url = state.url.clone();
+    let (mut socket, response) = connect(url).expect("Can't connect");
+    let mut http_response = String::new();
+    http_response.push_str(response.status().to_string().as_str());
+    http_response.push_str("\n\n");
+    for (key, value) in response.headers() {
+        http_response.push_str(key.as_str());
+        http_response.push_str(": ");
+        http_response.push_str(value.to_str().unwrap());
+        http_response.push('\n');
+    }
+
+    state.response = http_response;
 
     loop {
         terminal.draw(|f| ui(&mut state, f, f.area()))?;
         if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Char('q') => {
-                    socket.close(None).expect("bebra");
-                    break;
+            if key.modifiers.contains(KeyModifiers::CONTROL)
+            {
+                match key.code {
+                    KeyCode::Char('c') => {
+                        socket.close(None).expect("bebra");
+                        break;
+                    }
+                    KeyCode::Char('w') => {
+                        state.query = state.cached_query.clone();
+                        continue;
+                    }
+                    _ => {}
                 }
+            }
+            match key.code {
                 KeyCode::Char(c) => {
                     state.query.push(c);
                 }
@@ -36,18 +63,31 @@ fn main() -> Result<(), std::io::Error> {
                     state.query.pop();
                 }
                 KeyCode::Tab => {
-                    state.query = state.cached_query.clone();
+                    match state.mode {
+                        InputMode::URL => {
+                            state.mode = InputMode::Query;
+                        }
+                        InputMode::Query => {
+                            state.mode = InputMode::URL;
+                        }
+                    }
                 }
                 KeyCode::Enter => {
+                    if state.mode == InputMode::URL {
+                        continue;
+                    }
                     let req_text = format!("{}{}{}", TEMPLATE_BEGIN, state.query, TEMPLATE_END);
                     state.request = req_text.clone();
                     state.cached_query = state.query.clone();
                     state.query = "".to_string();
 
                     socket.send(Message::Text(req_text)).unwrap();
-                    loop{
+                    loop {
+                        if !socket.can_read() {
+                            break;
+                        }
                         let read = socket.read();
-                        match read{
+                        match read {
                             Ok(Message::Text(text)) => {
                                 state.response = text;
                                 break;
@@ -59,7 +99,6 @@ fn main() -> Result<(), std::io::Error> {
                             _ => {}
                         }
                     }
-
                 }
                 _ => {}
             }
@@ -71,43 +110,81 @@ fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
 
+#[derive(Clone, Copy, PartialEq)]
+enum InputMode{
+    URL,
+    Query
+}
+
 struct State {
+    url: String,
     query: String,
     cached_query: String,
     request: String,
-    response: String
+    response: String,
+    mode: InputMode
 }
 
 impl State {
     fn new() -> Self {
         Self {
+            url: "ws://127.0.0.1:3001/ws".to_string(),
             query: "".to_string(),
             cached_query: "".to_string(),
             request: "".to_string(),
-            response: "".to_string()
+            response: "".to_string(),
+            mode: InputMode::Query
         }
     }
 }
 
 fn ui(state: &mut State, f: &mut Frame, area: Rect) {
     let chunks = Layout::vertical([
-        Constraint::Length(3),
+        Constraint::Length(6),
         Constraint::Min(0),
         Constraint::Length(1),
     ])
     .flex(Flex::SpaceBetween)
     .split(area);
-
+    let input_chunks =
+        Layout::vertical([Constraint::Length(3), Constraint::Length(3)]).split(chunks[0]);
     let req_res_chunks =
         Layout::horizontal([Constraint::Percentage(50), Constraint::Percentage(50)])
             .split(chunks[1]);
 
-    // input field
+    // url field (currently does nothing)
     {
-        let block = Block::bordered().yellow().title("Input action");
-        let text = Paragraph::new(state.query.as_str()).yellow();
+        let mut block = Block::bordered().title("URL");
+        let mut text = Paragraph::new(state.url.as_str());
+        match state.mode {
+            InputMode::URL => {
+                block = block.yellow();
+                text = text.yellow();
+            }
+            InputMode::Query => {
+                block = block.white();
+                text = text.white();
+            }
+        }
         let input = text.block(block);
-        f.render_widget(input, chunks[0]);
+        f.render_widget(input, input_chunks[0]);
+    }
+    // query field
+    {
+        let mut block = Block::bordered().title("Input action");
+        let mut text = Paragraph::new(state.query.as_str());
+        match state.mode {
+            InputMode::URL => {
+                block = block.white();
+                text = text.white();
+            }
+            InputMode::Query => {
+                block = block.yellow();
+                text = text.yellow();
+            }
+        }
+        let input = text.block(block);
+        f.render_widget(input, input_chunks[1]);
     }
 
     // request block
@@ -123,14 +200,20 @@ fn ui(state: &mut State, f: &mut Frame, area: Rect) {
     // response block
     {
         let block = Block::bordered().green().title("Response");
-        let text = Paragraph::new(state.response.as_str()).green().wrap(Wrap::default());
+        let text = Paragraph::new(state.response.as_str())
+            .green()
+            .wrap(Wrap::default());
         let response = text.block(block);
         f.render_widget(response, req_res_chunks[1]);
     }
 
     // footer
     {
-        let footer = Paragraph::new("<Q> - quit, <Tab> - cached query, <Enter> - send").dim();
+        let text = match state.mode {
+            InputMode::URL => "<Ctrl + C> - quit, <Tab> - switch to query, <Ctrl + W> - cached input, <Enter> - send",
+            InputMode::Query => "<Ctrl + C> - quit, <Tab> - switch to URL, <Ctrl + W> - cached input, <Enter> - send",
+        };
+        let footer = Paragraph::new(text).dim();
         f.render_widget(footer, chunks[2]);
     }
 }
